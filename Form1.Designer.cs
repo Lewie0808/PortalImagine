4a76bc2dbe766159fdc78b72e8fa46a4cd8815ad
jlew0808@yahoo.com <jlew0808@yahoo.com>
2017-01-30 15:59:42 +0000

    V1 Code

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BrndLblBox = new System.Windows.Forms.Label();
+            this.BrndLocTxtBox = new System.Windows.Forms.TextBox();
+            this.BrndBrowBtn = new System.Windows.Forms.Button();
+            this.CompBrowBtn = new System.Windows.Forms.Button();
+            this.CmpLocTxtBox = new System.Windows.Forms.TextBox();
+            this.CompLblBox = new System.Windows.Forms.Label();
+            this.CopyBtn = new System.Windows.Forms.Button();
+            this.RevertBtn = new System.Windows.Forms.Button();
+            this.clseBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BrndLblBox
+            // 
+            this.BrndLblBox.AutoSize = true;
+            this.BrndLblBox.Location = new System.Drawing.Point(109, 9);
+            this.BrndLblBox.Name = "BrndLblBox";
+            this.BrndLblBox.Size = new System.Drawing.Size(62, 13);
+            this.BrndLblBox.TabIndex = 0;
+            this.BrndLblBox.Text = "Brand Logo";
+            this.BrndLblBox.Click += new System.EventHandler(this.LblBox_Click);
+            // 
+            // BrndLocTxtBox
+            // 
+            this.BrndLocTxtBox.Location = new System.Drawing.Point(28, 38);
+            this.BrndLocTxtBox.Name = "BrndLocTxtBox";
+            this.BrndLocTxtBox.Size = new System.Drawing.Size(143, 20);
+            this.BrndLocTxtBox.TabIndex = 1;
+            // 
+            // BrndBrowBtn
+            // 
+            this.BrndBrowBtn.Location = new System.Drawing.Point(188, 36);
+            this.BrndBrowBtn.Name = "BrndBrowBtn";
+            this.BrndBrowBtn.Size = new System.Drawing.Size(75, 23);
+            this.BrndBrowBtn.TabIndex = 2;
+            this.BrndBrowBtn.Text = "Browse";
+            this.BrndBrowBtn.UseVisualStyleBackColor = true;
+            this.BrndBrowBtn.Click += new System.EventHandler(this.BrndBrowBtn_Click);
+            // 
+            // CompBrowBtn
+            // 
+            this.CompBrowBtn.Location = new System.Drawing.Point(188, 101);
+            this.CompBrowBtn.Name = "CompBrowBtn";
+            this.CompBrowBtn.Size = new System.Drawing.Size(75, 23);
+            this.CompBrowBtn.TabIndex = 5;
+            this.CompBrowBtn.Text = "Browse";
+            this.CompBrowBtn.UseVisualStyleBackColor = true;
+            this.CompBrowBtn.Click += new System.EventHandler(this.CompBrowBtn_Click);
+            // 
+            // CmpLocTxtBox
+            // 
+            this.CmpLocTxtBox.Location = new System.Drawing.Point(28, 103);
+            this.CmpLocTxtBox.Name = "CmpLocTxtBox";
+            this.CmpLocTxtBox.Size = new System.Drawing.Size(143, 20);
+            this.CmpLocTxtBox.TabIndex = 4;
+            // 
+            // CompLblBox
+            // 
+            this.CompLblBox.AutoSize = true;
+            this.CompLblBox.Location = new System.Drawing.Point(109, 74);
+            this.CompLblBox.Name = "CompLblBox";
+            this.CompLblBox.Size = new System.Drawing.Size(78, 13);
+            this.CompLblBox.TabIndex = 3;
+            this.CompLblBox.Text = "Company Logo";
+            // 
+            // CopyBtn
+            // 
+            this.CopyBtn.Location = new System.Drawing.Point(12, 160);
+            this.CopyBtn.Name = "CopyBtn";
+            this.CopyBtn.Size = new System.Drawing.Size(121, 23);
+            this.CopyBtn.TabIndex = 6;
+            this.CopyBtn.Text = "Copy";
+            this.CopyBtn.UseVisualStyleBackColor = true;
+            this.CopyBtn.Click += new System.EventHandler(this.CopyBtn_Click);
+            // 
+            // RevertBtn
+            // 
+            this.RevertBtn.Location = new System.Drawing.Point(151, 160);
+            this.RevertBtn.Name = "RevertBtn";
+            this.RevertBtn.Size = new System.Drawing.Size(121, 23);
+            this.RevertBtn.TabIndex = 7;
+            this.RevertBtn.Text = "Revert";
+            this.RevertBtn.UseVisualStyleBackColor = true;
+            this.RevertBtn.Click += new System.EventHandler(this.RevertBtn_Click);
+            // 
+            // clseBtn
+            // 
+            this.clseBtn.Location = new System.Drawing.Point(97, 209);
+            this.clseBtn.Name = "clseBtn";
+            this.clseBtn.Size = new System.Drawing.Size(75, 23);
+            this.clseBtn.TabIndex = 8;
+            this.clseBtn.Text = "Close";
+            this.clseBtn.UseVisualStyleBackColor = true;
+            this.clseBtn.Click += new System.EventHandler(this.clseBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.clseBtn);
+            this.Controls.Add(this.RevertBtn);
+            this.Controls.Add(this.CopyBtn);
+            this.Controls.Add(this.CompBrowBtn);
+            this.Controls.Add(this.CmpLocTxtBox);
+            this.Controls.Add(this.CompLblBox);
+            this.Controls.Add(this.BrndBrowBtn);
+            this.Controls.Add(this.BrndLocTxtBox);
+            this.Controls.Add(this.BrndLblBox);
+            this.Name = "Form1";
+            this.Text = "Portal Imagine Change";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label BrndLblBox;
+        private System.Windows.Forms.TextBox BrndLocTxtBox;
+        private System.Windows.Forms.Button BrndBrowBtn;
+        private System.Windows.Forms.Button CompBrowBtn;
+        private System.Windows.Forms.TextBox CmpLocTxtBox;
+        private System.Windows.Forms.Label CompLblBox;
+        private System.Windows.Forms.Button CopyBtn;
+        private System.Windows.Forms.Button RevertBtn;
+        private System.Windows.Forms.Button clseBtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Portal
@@ -15,6 +8,76 @@ namespace Portal
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void LblBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BrndBrowBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "C# Corner Open File Dialog";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+
+
+            {
+                BrndLocTxtBox.Text = fdlg.FileName;
+            }
+        }
+
+        private void CompBrowBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "C# Corner Open File Dialog";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                CmpLocTxtBox.Text = fdlg.FileName;
+            }
+        }
+
+        private void clseBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CopyBtn_Click(object sender, EventArgs e)
+        {
+            string brnSourcePath = BrndLocTxtBox.Text;
+            string brndestinationPath = @"C:\Portal\Client\img\";
+            string brndestinationFilename = "brand_logo.png";
+            string brnSourceFile = System.IO.Path.GetFullPath(brnSourcePath);
+            string brndestinationFile = System.IO.Path.Combine(brndestinationPath, brndestinationFilename);
+
+            string cmpSourcePath = CmpLocTxtBox.Text;
+            string cmpdestinationPath = @"C:\Portal\Client\img\";
+            string cmpdestinationFilename = "company_logo.png";
+            string cmpsourceFile = System.IO.Path.GetFullPath(cmpSourcePath);
+            string cmpdestinationFile = System.IO.Path.Combine(cmpdestinationPath, cmpdestinationFilename);
+           
+            System.IO.File.Copy(brnSourceFile, brndestinationFile, true);
+                                    
+            System.IO.File.Copy(cmpsourceFile, cmpdestinationFile, true);
+
+            MessageBox.Show("Copy complete", "Copy", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+         }
+
+        private void RevertBtn_Click(object sender, EventArgs e)
+        {
+            
+            System.IO.File.Copy(@"C:\Portal\Client\img\Original\brand_logo.png", @"C:\Portal\Client\img\brand_logo.png", true);
+            System.IO.File.Copy(@"C:\Portal\Client\img\Original\company_logo.png", @"C:\Portal\Client\img\company_logo.png", true);
+                     
+            MessageBox.Show("Reverted back", "Reverted", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
         }
     }
 }
