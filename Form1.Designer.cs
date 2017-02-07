86e939c3fcefa509582274274eb0ca4331990501
jlew0808@yahoo.com <jlew0808@yahoo.com>
2017-02-07 14:55:34 +0000

    Added drag & drop plus buttons to image folder and client config file

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -41,12 +41,18 @@
             this.firmNameTxtbx = new System.Windows.Forms.TextBox();
             this.firmUrlTxtbx = new System.Windows.Forms.TextBox();
             this.urlLbl = new System.Windows.Forms.Label();
+            this.Rename = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ImgOpenBtn = new System.Windows.Forms.Button();
+            this.ConfigFileBtn = new System.Windows.Forms.Button();
+            this.Rename.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // BrndLblBox
             // 
             this.BrndLblBox.AutoSize = true;
-            this.BrndLblBox.Location = new System.Drawing.Point(115, 127);
+            this.BrndLblBox.Location = new System.Drawing.Point(100, 3);
             this.BrndLblBox.Name = "BrndLblBox";
             this.BrndLblBox.Size = new System.Drawing.Size(62, 13);
             this.BrndLblBox.TabIndex = 0;
@@ -55,14 +61,15 @@
             // 
             // BrndLocTxtBox
             // 
-            this.BrndLocTxtBox.Location = new System.Drawing.Point(18, 158);
+            this.BrndLocTxtBox.AllowDrop = true;
+            this.BrndLocTxtBox.Location = new System.Drawing.Point(3, 34);
             this.BrndLocTxtBox.Name = "BrndLocTxtBox";
             this.BrndLocTxtBox.Size = new System.Drawing.Size(143, 20);
             this.BrndLocTxtBox.TabIndex = 1;
             // 
             // BrndBrowBtn
             // 
-            this.BrndBrowBtn.Location = new System.Drawing.Point(178, 156);
+            this.BrndBrowBtn.Location = new System.Drawing.Point(163, 32);
             this.BrndBrowBtn.Name = "BrndBrowBtn";
             this.BrndBrowBtn.Size = new System.Drawing.Size(75, 23);
             this.BrndBrowBtn.TabIndex = 2;
@@ -72,7 +79,7 @@
             // 
             // CompBrowBtn
             // 
-            this.CompBrowBtn.Location = new System.Drawing.Point(178, 221);
+            this.CompBrowBtn.Location = new System.Drawing.Point(163, 97);
             this.CompBrowBtn.Name = "CompBrowBtn";
             this.CompBrowBtn.Size = new System.Drawing.Size(75, 23);
             this.CompBrowBtn.TabIndex = 5;
@@ -82,7 +89,8 @@
             // 
             // CmpLocTxtBox
             // 
-            this.CmpLocTxtBox.Location = new System.Drawing.Point(18, 223);
+            this.CmpLocTxtBox.AllowDrop = true;
+            this.CmpLocTxtBox.Location = new System.Drawing.Point(3, 99);
             this.CmpLocTxtBox.Name = "CmpLocTxtBox";
             this.CmpLocTxtBox.Size = new System.Drawing.Size(143, 20);
             this.CmpLocTxtBox.TabIndex = 4;
@@ -90,7 +98,7 @@
             // CompLblBox
             // 
             this.CompLblBox.AutoSize = true;
-            this.CompLblBox.Location = new System.Drawing.Point(100, 191);
+            this.CompLblBox.Location = new System.Drawing.Point(85, 67);
             this.CompLblBox.Name = "CompLblBox";
             this.CompLblBox.Size = new System.Drawing.Size(78, 13);
             this.CompLblBox.TabIndex = 3;
@@ -98,9 +106,9 @@
             // 
             // CopyBtn
             // 
-            this.CopyBtn.Location = new System.Drawing.Point(18, 271);
+            this.CopyBtn.Location = new System.Drawing.Point(15, 302);
             this.CopyBtn.Name = "CopyBtn";
-            this.CopyBtn.Size = new System.Drawing.Size(121, 23);
+            this.CopyBtn.Size = new System.Drawing.Size(76, 23);
             this.CopyBtn.TabIndex = 6;
             this.CopyBtn.Text = "Copy";
             this.CopyBtn.UseVisualStyleBackColor = true;
@@ -108,9 +116,9 @@
             // 
             // RevertBtn
             // 
-            this.RevertBtn.Location = new System.Drawing.Point(157, 271);
+            this.RevertBtn.Location = new System.Drawing.Point(103, 302);
             this.RevertBtn.Name = "RevertBtn";
-            this.RevertBtn.Size = new System.Drawing.Size(121, 23);
+            this.RevertBtn.Size = new System.Drawing.Size(75, 23);
             this.RevertBtn.TabIndex = 7;
             this.RevertBtn.Text = "Revert";
             this.RevertBtn.UseVisualStyleBackColor = true;
@@ -118,7 +126,7 @@
             // 
             // clseBtn
             // 
-            this.clseBtn.Location = new System.Drawing.Point(103, 320);
+            this.clseBtn.Location = new System.Drawing.Point(61, 331);
             this.clseBtn.Name = "clseBtn";
             this.clseBtn.Size = new System.Drawing.Size(75, 23);
             this.clseBtn.TabIndex = 8;
@@ -129,7 +137,7 @@
             // firmnameLbl
             // 
             this.firmnameLbl.AutoSize = true;
-            this.firmnameLbl.Location = new System.Drawing.Point(120, 9);
+            this.firmnameLbl.Location = new System.Drawing.Point(107, 16);
             this.firmnameLbl.Name = "firmnameLbl";
             this.firmnameLbl.Size = new System.Drawing.Size(57, 13);
             this.firmnameLbl.TabIndex = 9;
@@ -137,7 +145,7 @@
             // 
             // firmNameTxtbx
             // 
-            this.firmNameTxtbx.Location = new System.Drawing.Point(18, 30);
+            this.firmNameTxtbx.Location = new System.Drawing.Point(5, 37);
             this.firmNameTxtbx.Name = "firmNameTxtbx";
             this.firmNameTxtbx.Size = new System.Drawing.Size(235, 20);
             this.firmNameTxtbx.TabIndex = 10;
@@ -145,7 +153,7 @@
             // 
             // firmUrlTxtbx
             // 
-            this.firmUrlTxtbx.Location = new System.Drawing.Point(18, 95);
+            this.firmUrlTxtbx.Location = new System.Drawing.Point(5, 102);
             this.firmUrlTxtbx.Name = "firmUrlTxtbx";
             this.firmUrlTxtbx.Size = new System.Drawing.Size(235, 20);
             this.firmUrlTxtbx.TabIndex = 11;
@@ -155,34 +163,80 @@
             // urlLbl
             // 
             this.urlLbl.AutoSize = true;
-            this.urlLbl.Location = new System.Drawing.Point(120, 68);
+            this.urlLbl.Location = new System.Drawing.Point(107, 75);
             this.urlLbl.Name = "urlLbl";
             this.urlLbl.Size = new System.Drawing.Size(42, 13);
             this.urlLbl.TabIndex = 12;
             this.urlLbl.Text = "Firm Url";
             // 
+            // Rename
+            // 
+            this.Rename.Controls.Add(this.firmnameLbl);
+            this.Rename.Controls.Add(this.urlLbl);
+            this.Rename.Controls.Add(this.firmNameTxtbx);
+            this.Rename.Controls.Add(this.firmUrlTxtbx);
+            this.Rename.Location = new System.Drawing.Point(13, 13);
+            this.Rename.Name = "Rename";
+            this.Rename.Size = new System.Drawing.Size(258, 136);
+            this.Rename.TabIndex = 13;
+            this.Rename.TabStop = false;
+            this.Rename.Text = "Rename";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BrndLocTxtBox);
+            this.groupBox1.Controls.Add(this.BrndLblBox);
+            this.groupBox1.Controls.Add(this.BrndBrowBtn);
+            this.groupBox1.Controls.Add(this.CompLblBox);
+            this.groupBox1.Controls.Add(this.CmpLocTxtBox);
+            this.groupBox1.Controls.Add(this.CompBrowBtn);
+            this.groupBox1.Location = new System.Drawing.Point(15, 158);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 128);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Logo";
+            // 
+            // ImgOpenBtn
+            // 
+            this.ImgOpenBtn.Location = new System.Drawing.Point(195, 302);
+            this.ImgOpenBtn.Name = "ImgOpenBtn";
+            this.ImgOpenBtn.Size = new System.Drawing.Size(75, 23);
+            this.ImgOpenBtn.TabIndex = 15;
+            this.ImgOpenBtn.Text = "Img folder";
+            this.ImgOpenBtn.UseVisualStyleBackColor = true;
+            this.ImgOpenBtn.Click += new System.EventHandler(this.ImgOpenBtn_Click);
+            // 
+            // ConfigFileBtn
+            // 
+            this.ConfigFileBtn.Location = new System.Drawing.Point(154, 331);
+            this.ConfigFileBtn.Name = "ConfigFileBtn";
+            this.ConfigFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConfigFileBtn.TabIndex = 16;
+            this.ConfigFileBtn.Text = "Config File";
+            this.ConfigFileBtn.UseVisualStyleBackColor = true;
+            this.ConfigFileBtn.Click += new System.EventHandler(this.ConfigFileBtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(285, 351);
-            this.Controls.Add(this.urlLbl);
-            this.Controls.Add(this.firmUrlTxtbx);
-            this.Controls.Add(this.firmNameTxtbx);
-            this.Controls.Add(this.firmnameLbl);
+            this.ClientSize = new System.Drawing.Size(281, 362);
+            this.Controls.Add(this.ConfigFileBtn);
+            this.Controls.Add(this.ImgOpenBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Rename);
             this.Controls.Add(this.clseBtn);
             this.Controls.Add(this.RevertBtn);
             this.Controls.Add(this.CopyBtn);
-            this.Controls.Add(this.CompBrowBtn);
-            this.Controls.Add(this.CmpLocTxtBox);
-            this.Controls.Add(this.CompLblBox);
-            this.Controls.Add(this.BrndBrowBtn);
-            this.Controls.Add(this.BrndLocTxtBox);
-            this.Controls.Add(this.BrndLblBox);
             this.Name = "Form1";
             this.Text = "Portal Imagine Change";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Rename.ResumeLayout(false);
+            this.Rename.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -201,6 +255,10 @@
         private System.Windows.Forms.TextBox firmNameTxtbx;
         private System.Windows.Forms.TextBox firmUrlTxtbx;
         private System.Windows.Forms.Label urlLbl;
+        private System.Windows.Forms.GroupBox Rename;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ImgOpenBtn;
+        private System.Windows.Forms.Button ConfigFileBtn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Windows.Forms;
 
@@ -62,7 +63,7 @@ namespace Portal
             {
                 File.WriteAllText(@"C:\Portal\client\config.js", File.ReadAllText(@"C:\Portal\client\config.js").Replace("One Advanced", firmNameTxtbx.Text));
             }
-            
+
             if (string.IsNullOrWhiteSpace(firmUrlTxtbx.Text))
             {
                 MessageBox.Show("Please add Url Address", "URLAddress", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
@@ -72,7 +73,7 @@ namespace Portal
             {
                 File.WriteAllText(@"C:\Portal\client\config.js", File.ReadAllText(@"C:\Portal\client\config.js").Replace("https://www.oneadvanced.com/", firmUrlTxtbx.Text));
             }
-            
+
             if (string.IsNullOrWhiteSpace(BrndLocTxtBox.Text))
             {
                 MessageBox.Show("Please add Brand Logo", "BrandLogo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
@@ -88,7 +89,7 @@ namespace Portal
 
                 System.IO.File.Copy(brnSourceFile, brndestinationFile, true);
             }
-            
+
             if (string.IsNullOrWhiteSpace(CmpLocTxtBox.Text))
             {
                 MessageBox.Show("Please add Company Logo", "BrandLogo", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
@@ -106,16 +107,16 @@ namespace Portal
             }
 
             MessageBox.Show("Name, URL, Logo's changed", "Copy", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
-         }
+        }
 
         private void RevertBtn_Click(object sender, EventArgs e)
         {
-            
+
             System.IO.File.Copy(@"C:\Portal\Client\img\Original\brand_logo.png", @"C:\Portal\Client\img\brand_logo.png", true);
             System.IO.File.Copy(@"C:\Portal\Client\img\Original\company_logo.png", @"C:\Portal\Client\img\company_logo.png", true);
 
             //Change company back to One Advanced
-            
+
             string textName = File.ReadAllText(@"C:\Portal\client\config.js");
 
             var firmnameString = "var FIRMNAME = '";
@@ -130,26 +131,87 @@ namespace Portal
             textName = textName.Replace(theStringName, newValueName);
 
             File.WriteAllText(@"C:\Portal\client\config.js", textName);
-            
+
             //Change company URL to One Advanced
-                                 
+
             string textUrl = File.ReadAllText(@"C:\Portal\client\config.js");
-                        
+
             var firmUrlString = "var FIRMURL = '";
             int pUrl = textUrl.IndexOf(firmUrlString) + firmUrlString.Length;
 
             string rest = textUrl.Substring(pUrl, textUrl.Length - pUrl);
             int lengthOfStringUrl = rest.IndexOf("'");
-         
+
             string theStringUrl = textUrl.Substring(pUrl, lengthOfStringUrl);
             var newValueUrl = "https://www.oneadvanced.com/";
-                
+
             textUrl = textUrl.Replace(theStringUrl, newValueUrl);
-            
+
             File.WriteAllText(@"C:\Portal\client\config.js", textUrl);
 
             MessageBox.Show("Defaulted to One Advanced", "Revert", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BrndLocTxtBox.AllowDrop = true;
+            BrndLocTxtBox.DragEnter += new DragEventHandler(BrndLocTxtBox_DragEnter);
+            BrndLocTxtBox.DragDrop += new DragEventHandler(BrndLocTxtBox_DragDrop);
+
+            CmpLocTxtBox.AllowDrop = true;
+            CmpLocTxtBox.DragEnter += new DragEventHandler(CmpLocTxtBox_DragEnter);
+            CmpLocTxtBox.DragDrop += new DragEventHandler(CmpLocTxtBox_DragDrop);
+        }
+
+        private void BrndLocTxtBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+               e.Effect = DragDropEffects.Copy;
+                          
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void BrndLocTxtBox_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            string s = "";
+
+            foreach (string File in FileList)
+                s = s + " " + File;
+            BrndLocTxtBox.Text = s;
+        }
+
+        
+        private void CmpLocTxtBox_DragEnter(object sender, DragEventArgs e)
+    {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+
+            else
+                e.Effect = DragDropEffects.None;
+    }
+
+        private void CmpLocTxtBox_DragDrop(object sender, DragEventArgs e)
+    {
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+            string s = "";
+
+            foreach (string File in FileList)
+                s = s + " " + File;
+            CmpLocTxtBox.Text = s;
+    }
+
+        private void ImgOpenBtn_Click(object sender, EventArgs e)
+        {
+            Process.Start(@"C:\Portal\client\img");
+        }
+
+        private void ConfigFileBtn_Click(object sender, EventArgs e)
+        {
+            Process.Start("notepad.exe", @"C:\Portal\client\config.js");
+        }
     }
 }
